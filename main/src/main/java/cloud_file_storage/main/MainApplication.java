0b638e79f74d82f9875646ac465d831f48264a7a
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-07-19 15:47:17 +0300

    feat: init backend

---
+++ main/src/main/java/cloud_file_storage/main/MainApplication.java
@@ -0,0 +1,14 @@
+package cloud_file_storage.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class MainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+}
