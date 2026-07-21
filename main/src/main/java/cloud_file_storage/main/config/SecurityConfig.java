51b5b6611243cd8f455614b8c3c47b00744430a7
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-07-22 00:38:26 +0300

    feat: add exception handler to security config

--- main/src/main/java/cloud_file_storage/main/config/SecurityConfig.java
+++ main/src/main/java/cloud_file_storage/main/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package cloud_file_storage.main.config;
 
 import cloud_file_storage.main.session.SessionFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,7 +30,18 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .addFilterBefore(sessionFilter, UsernamePasswordAuthenticationFilter.class);
+        .addFilterBefore(sessionFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(
+            ex ->
+                ex.authenticationEntryPoint(
+                    (request, response, authException) -> {
+                      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                      response.setCharacterEncoding("UTF-8");
+                      response
+                          .getWriter()
+                          .write("{\"status\":401,\"message\":\"Неавторизован или токен истёк\"}");
+                    }));
 
     return http.build();
   }
