41a70be06a53ab0027eb6f4a141ab9491ddf1abf
Kamil Khuzzyatov <kkhuzzyatov@gmail.com>
2026-07-21 23:27:01 +0300

    feat: add Spring Security config

---
+++ main/src/main/java/cloud_file_storage/main/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package cloud_file_storage.main.config;
+
+import cloud_file_storage.main.session.SessionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final SessionFilter sessionFilter;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) {
+    http.csrf(csrf -> csrf.disable())
+        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .requestMatchers("/swagger-ui/**", "/api-docs/**", "/openapi.yml")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .addFilterBefore(sessionFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
